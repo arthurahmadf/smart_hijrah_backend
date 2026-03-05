e60449b6a8980c28c960094826753df0d5b84f83
arthurahmadf <arthurahmadf@gmail.com>
2026-03-05 13:46:55 +0700

    added profile picture and its changing function

--- main/views.py
+++ main/views.py
@@ -9,6 +9,8 @@ from django.utils.timezone import now, timedelta
 from django.utils import timezone
 import json
 from .models import *
+from . import my_utils as helper
+from firebase_admin import messaging, credentials
 
 # Create your views here.
 # FIREBASE OJOK DIGANTI BANG CEK PENAK LEK COPAS COPAS
@@ -136,15 +138,13 @@ def users_me(request):
             "name":user.nama,
             "email":user.email,
             "alamat":user.alamat,
-            "jabatan":user.jabatan,
-            
+            "profile_picture": helper.generate_url(user.foto_profil,request)
         }
         return JsonResponse({"success":True,"data":user_data}, status=200)
     except Exception as e:
         return JsonResponse({"success": False, "message": str(e)}, status=400)
     
-@api_view(["POST"])
-@permission_classes([AllowAny])
+@api_view(['POST'])
 def create_user(request):
     try:
         data = json.loads(request.body)
@@ -155,8 +155,60 @@ def create_user(request):
             alamat = data.get("alamat"),
             telepon = data.get("telepon"),
         )
+        foto_profil_base64 = data.get("foto_profil")
+        if foto_profil_base64:
+            user.foto_profil = helper.base64_to_image_file(foto_profil_base64,"profil")
+            
         user.set_password(data.get("password"))
         user.save()
         return JsonResponse({"success": True, "id": user.id, "message": "User verhasil dibuat"}, status=200)
     except Exception as e:
         return JsonResponse({"success": False, "message": str(e)}, status=400)
+
+@api_view(['PATCH'])
+@permission_classes([IsAuthenticated])
+def change_profile_picture(request):
+    try:
+        data = json.loads(request.body)
+        foto_base64 = data.get("foto_profil")
+
+        if not foto_base64:
+            return JsonResponse(
+                {"success": False, "message": "Foto Profil tidak ditemukan"},
+                status=400
+            )
+
+        user = request.user
+
+        if user.foto_profil:
+            user.foto_profil.delete(save=False)
+
+        user.foto_profil = helper.base64_to_image_file(foto_base64, "profil")
+        user.save()
+
+        return JsonResponse(
+            {"success": True, "message": "Foto Profil berhasil diubah"},
+            status=200
+        )
+
+    except Exception as e:
+        return JsonResponse(
+            {"success": False, "message": str(e)},
+            status=400
+        )
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def change_password(request):
+    if request.method == "PATCH":
+        try:
+            data = json.loads(request.body)
+            user = get_object_or_404(User, id=data.get("user_id"))
+            password = data.get("password")
+            user.set_password(password)
+            user.save()
+            return JsonResponse({"success": True, "message": "Password updated"}, status=200)
+        except Exception as e:
+            return JsonResponse({"success": False, "message": str(e)}, status=400)
+    return JsonResponse({"success": False, "message": "Invalid method"}, status=405)
+
